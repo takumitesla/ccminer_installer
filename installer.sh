d76c5e9a34074fde86ce4478d7f1fcaabeaaa433
takumitesla <takumitesla@gmail.com>
2025-05-07 19:37:06 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+clear
+pkg install figlet -y
+clear
 # Fungsi untuk meminta input dengan default
 function input_with_default() {
     local prompt="$1"
@@ -12,17 +14,19 @@ function input_with_default() {
     fi
 }
 
-
+figlet CCMINER INSTALLER
+echo "by Takumi Tesla"
+echo "github : https://github.com/takumitesla/"
 # Meminta input untuk wallet dan worker
 wallet=$(input_with_default "Masukkan Wallet (default: RUjugNHKHCxKxFZinvhRuurLjK8iBkTL6a): " "RUjugNHKHCxKxFZinvhRuurLjK8iBkTL6a")
 worker=$(input_with_default "Masukkan Nama Worker (contoh: Redmi4x1): " "worker")
 threads=$(input_with_default "Masukkan Jumlah Threads (contoh: 8): " "8")
-
+clear
 # Menginstal paket yang diperlukan
 pkg install libjansson nano -y
 
 # Membuat direktori dan berpindah ke dalamnya
-mkdir -p ccminer && cd ccminer
+mkdir -p ~/ccminer && cd ~/ccminer
 
 # Mengunduh file ccminer dan start.sh
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
@@ -54,4 +58,5 @@ chmod +x ~/ccminer/ccminer
 # Menambahkan perintah untuk menjalankan start.sh saat membuka Termux
 echo "~/ccminer/ccminer -c ~/ccminer/config.json" >> ~/.bashrc
 # Memberikan informasi selesai
+clear
 echo "Instalasi selesai!"
